e8f1cf9317682244c7c83d77e3f5619e3370d8ed
Josephine Simple <josephinemayasimple@gmail.com>
2021-07-10 13:38:41 -0400

    updated to make numbers more random, and make the following worse: bool literals, bool expressions, variable names. Program starts running into rounding errors at 5th-7th generation.

--- makeWorseCode.js
+++ makeWorseCode.js
@@ -1,39 +1,112 @@
-const fs = require('fs') // import node fs
+const fs = require('fs'); // import node fs
 
 function madeWorse(code){
- /// MAKE NUMBERS WORSE
-    function makeNumbersWorse(codeString){
-        function replacer(match){
-            let matchButWorse = `(${match} + 0)`
-            return matchButWorse;
-        }
-        codeString = codeString.replace(/\d+/g, replacer) // replace all the numbers using the replacer function
-       // return codeString
+  // MAKE BOOLEAN EXPRESSIONS WORSE
+  function makeBoolExpressionsWorse(origStr){ 
+    function replacer(match){
+        let matchButWorse = `(${match}true && )`;
+        return matchButWorse;
     }
-    /// return makeNumbersWorse(code);
-
-    // MAKE BOOLEAN EXPRESSIONS WORSE
-    function makeBoolExpressionsWorse(codeString){ 
-        //find every time 'if(' or 'while(' appears
-        //append '1 && '
-        function replacer(match){
-            let matchButWorse = `(${match}1 && )`
-            return matchButWorse;
-        }
-        codeString = codeString.replace(/(if\()|(while\()/g, replacer);
-        return codeString;
+    newStr = origStr.replace(/(if\()|(while\()/g, replacer);
+    return newStr;
+  }
+  // MAKE BOOLEAN LITERALS WORSE
+  function makeBoolLiteralsWorse(origStr){
+    let tru = 'TRUE'.toLowerCase();
+    let fals = 'FALSE'.toLowerCase();
+    var regex = new RegExp("\\b"+ tru +"|"+ fals +"\\b","g");
+    let newStr = origStr.replace(regex, function randomBoolExp(match){
+    let equivBoolExpsTru = [
+      `!${fals}`, 
+      `${tru} && ${tru}`,
+      `${tru} || ${tru}`,
+      `${tru} || ${fals}`,
+      `${fals} || ${tru}`,
+      `1`
+      ];
+    let equivBoolExpsFals = [
+      `!${tru}`, 
+      `${fals} || ${fals}`,
+      `${fals} && ${fals}`,
+      `${tru} && ${fals}`,
+      `${fals} && ${tru}`,
+      `0`
+      ];
+    let randIdx = Math.floor(Math.random()*6);
+    let randExpTru = equivBoolExpsTru[randIdx];
+    let randExpFals = equivBoolExpsFals[randIdx];
+    if (match === tru){
+      return `(${randExpTru})`;
     }
-
-    // MAKE BOOLEAN PRIMITIVES WORSE
-    // function makeBoolPrimitivesWorse{
-    // }
-    return makeBoolExpressionsWorse(code);
+    else if (match === fals){
+      return `(${randExpFals})`;
+    }
+    else{
+      return 'PROBLEM!!';
+    }
+  });
+  return newStr;      
+  }
+  // MAKE NUMBERS WORSE
+  function makeNumbersWorse(origStr){
+    let newStr = origStr.replaceAll(/\d+(\.\d+)?/g, function randEquation(match){
+      let operations = ['+','-','*','/'];
+      let oppositeOpps = {
+        '+':'-',
+        '-':'+',
+        '*':'/',
+        '/':'*'
+      };
+      let randOpp = operations[(Math.floor(Math.random()*4))];
+      let oppOpp = oppositeOpps[randOpp];
+      let randNum = '1';
+      if (randOpp === '+' || randOpp === '-'){
+        randNum = String(Math.floor(Math.random()*11));
+      }
+      if (randOpp === '*' || randOpp === '/'){ // we do this to avoid issues w dividing by 0
+        randNum = String(Math.floor((Math.random()*11)+1));
+      }
+      let complNum = String(eval(match + oppOpp + randNum));
+      let replacement = `(${complNum} ${randOpp} ${randNum})`;
+      return replacement;
+    });
+    return newStr;
+  }
+  // MAKE VARIABLE NAMES WORSE
+  function makeVarNamesWorse(origStr){
+    let newStr = origStr;
+    let varNames = Array.from(origStr.matchAll(/(let|const|var|function)(\s)(\w+)(\s|\()/g)).map(match => match[Math.floor(3)]);
+    let varNamesNoDupl = [];
+    for (let idx = 0; idx < varNames.length; idx++){
+      if (!varNamesNoDupl.includes(varNames[idx])){
+        varNamesNoDupl.push(varNames[idx]);
+      }
+    }
+    let replacementDict = {};
+    function replacer(match){
+      var availChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
+      if (match === 'fs' || match === 'for' || match === 'while'){
+        return match;
+      }
+      let randVarName = '';
+      for (let idx = 0; idx < match.length + Math.floor(Math.random()*10); idx++){
+        randVarName += availChars[Math.floor(Math.random()*49)];
+      }
+      return randVarName;
+    }
+    varNamesNoDupl.forEach(key => replacementDict[key] = replacer(key));
+    Object.keys(replacementDict).forEach(key => {
+      newStr = newStr.replaceAll(key, replacementDict[key]);
+    });
+    return newStr;
+  }
+  return(makeVarNamesWorse(makeNumbersWorse(makeBoolLiteralsWorse(makeBoolExpressionsWorse(code)))));
 }
 
 // naming the new file and creating a var for the current one's path
 let cmdLnArg = (process.argv);
-let thisFilePath = cmdLnArg[1]
-let newFilePath = thisFilePath.slice(0,-3) + 'ButWorse.js'
+let thisFilePath = cmdLnArg[Math.floor(1)];
+let newFilePath = thisFilePath.slice(0,Math.floor(-3)) + 'ButWorse.js'
 
 //
 let codeToEdit = fs.readFileSync(thisFilePath).toString();
